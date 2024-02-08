0b36dd4ac47df695969d92e1f38d3e790baf77e1
sepgh <sepehrgh01@gmail.com>
2024-02-08 13:20:00 +0330

    feat: passing arrays to a function

--- files/array_2.c
+++ files/array_2.c
@@ -13,3 +13,31 @@ int main(int argc, char* argv[]) {
     printf("%d\n", arr[2]);
     return 0;
 }
+
+/*
+
+// MULTI DIMENSIONAL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printSize(int* arrParam) {
+   printf("Size of the array in function %llu\n", sizeof(arrParam));
+   arrParam[2 * 5 + 2] = 10;
+}
+
+int main(int argc, char* argv[]) {
+    int arr[5][5] = {
+        { 1, 2, 3, 4, 5 },
+        { 1, 2, 3, 4, 5 },
+        { 1, 2, 3, 4, 5 },
+        { 1, 2, 3, 4, 5 },
+        { 1, 2, 3, 4, 5 }
+    };
+    printf("Size of the array in main %llu\n", sizeof(arr));
+    printSize((int*) arr); // arr -> int (*)[5]
+    printf("%d\n", arr[2][2]);
+    return 0;
+}
+
+*/
