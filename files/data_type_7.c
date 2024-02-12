7bf2b878e971f299061a542bbb5fdc2b39acced8
sepgh <sepehrgh01@gmail.com>
2024-02-12 13:30:41 +0330

    feat: macros

--- files/data_type_7.c
+++ files/data_type_7.c
@@ -8,7 +8,7 @@ const char* PATTERN2 = "My name is ";
 
 int main(int argc, char* argv[]) {
     printf(PATTERN"Bob\n"); // Works
-    printf(PATTERN2"Bob\n"); // Breaks, since PATTERN2 is a pointer
+    // printf(PATTERN2"Bob\n"); // Breaks, since PATTERN2 is a pointer
     
     return 0;
 }
